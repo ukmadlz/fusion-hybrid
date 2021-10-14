01388bf5c44affb995aed9815f02c3fc4892c0f5
Mike Elsmore <mike@elsmore.me>
2021-10-14 15:29:30 +0100

    Title screen

--- overlays/pages/title.tsx
+++ overlays/pages/title.tsx
@@ -1,17 +1,29 @@
 /* eslint-disable @next/next/no-img-element */
 import HeadComponent from '../components/head.component'
-import FooterComponent from '../components/footer.component'
 import CarouselComponent from '../components/carousel.component'
+
+const thirdWidth = (1920/3);
+const thirdHeight = (1080/3);
 
 export default function Home() {
   return (
     <div className="container">
       <HeadComponent/>
       <main>
-        <img
-            src="/images/FUSION-EVENTS.png"
-            alt="Fusion Events"
-        />
+        <div
+            style={{
+                width: thirdWidth + 'px',
+                height: thirdHeight + 'px',
+                position: 'absolute',
+                left: thirdWidth + 'px',
+                top: (thirdHeight - 100) + 'px',
+            }}>
+            <img
+                src="/images/FUSION-EVENTS.png"
+                alt="Fusion Events"
+                style={{width: '100%'}}
+            />
+        </div>
       </main>
     </div>
   )
